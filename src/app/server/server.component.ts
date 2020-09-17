09602d387f6a679cb8360abbca9dbf11a5d01e58
nivinjohn37 <getnivin@gmail.com>
2020-09-17 15:55:12 +0400

    17th

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-selector:'app-server',
-templateUrl:'./server.component.html'
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+    serverId: number = 10;
+    serverStatus: string = 'online';
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { style } from '@angular/animations';
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `<p>Warning Alert Test</p>`,
+    styles: [`
+    p{
+        padding: 20px;
+        background-color:mistyrose;
+        border: 1px solid red;
+    }`]
+})
+export class WarningAlertComponent {
+
+}
